5747637cff7679d36a51b4f2a841a53a50b66cd0
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-07-31 14:44:58 +0530

    Update page.jsx

--- app/app/myproperty/page.jsx
+++ app/app/myproperty/page.jsx
@@ -4,16 +4,23 @@ import { cookies } from 'next/headers';
 import { searchMypro } from '@/actions/property';
 import Footer7 from '@/components/footer7';
 import PropertyNavbar from '@/components/navbar5';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
 
     const cookieStore = cookies();
       const user = cookieStore.get("user")?.value;
 
+       if(!user){
+    redirect(`/login`)
+  }
+
     const id = JSON.parse(user);
   console.log("User ID:", id);
 
   const property = await searchMypro(id)
+
+ 
 
   console.log("Property Data:", property);
 
